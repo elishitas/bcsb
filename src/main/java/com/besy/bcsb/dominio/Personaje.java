7f313c3c2eaabafa5b1285b4297accdc4bccf4f0
Elishitas <arayamariaelisa@gmail.com>
2023-01-20 11:38:38 -0300

    dominio

---
+++ src/main/java/com/besy/bcsb/dominio/Personaje.java
@@ -0,0 +1,39 @@
+package com.besy.bcsb.dominio;
+
+public class Personaje {
+    private String nombre;
+    private int edad;
+    private Float peso;
+    private String historia;
+
+    public Personaje(String nombre, int edad, double peso, String historia) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.historia = historia;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public String getHistoria() {
+        return historia;
+    }
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+}
